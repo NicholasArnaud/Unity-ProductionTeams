c7b9bae5fb91ef0ffdcbab929994d3d23c8fed10
BlaneTorregrossa <blanet97@cox.net>
2017-05-26 17:45:57 -0500

    Start Work on Buttons for Menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//For Buttons on Menu
+public class MenuController : MonoBehaviour
+{
+    public Button theButton;
+   
+    private string CurrentCase;
+
+    //Actions for buttons being clicked
+    public bool GetButtonInput()
+    {
+        
+        return false;
+    }
+
+    //The actions of the buttons
+    public string DoButtonAction()
+    {
+        switch (CurrentCase)
+        {
+            case "Start":
+
+            case "Credits":
+
+            case "Quit":
+
+            default:
+                return null;
+        }
+
+    }
+
+}
