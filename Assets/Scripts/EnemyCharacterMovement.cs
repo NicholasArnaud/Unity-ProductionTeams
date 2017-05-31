beb00855fe050ec52bc97f6dd8d917dc0d638596
Nicholas Arnaud <cobrahead09@gmail.com>
2017-05-31 16:58:49 -0500

    movement and animation controller

---
+++ Assets/Scripts/EnemyCharacterMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Animator), typeof(Rigidbody), typeof(NavMeshAgent))]
+public class EnemyCharacterMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator animator;
+    private Rigidbody rb;
+    private readonly int SPEED = Animator.StringToHash("speed");
+    public Transform target;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.destination = target.position;
+        animator.SetFloat(SPEED, agent.velocity.magnitude);
+    }
+
+}
