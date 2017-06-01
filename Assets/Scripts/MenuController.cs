e6e1f3896b7840eb569eab347dee69c42334ad5e
BlaneTorregrossa <blanet97@cox.net>
2017-06-01 17:16:51 -0500

    5:16 P.M. 6/1/2017

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -8,104 +8,54 @@ using UnityEngine.Events;
 public class MenuController : MonoBehaviour
 {
     [HideInInspector]
-    public Button theButton;
+    public Button TheButton;
 
-    public Canvas buttonCanvas;
-    public GameObject buttonObject;
-    public Text buttonText;
-    public Text testText;
-
-    private string CurrentCase;
-    private int currentButtonPos;
-
-    //Actions for buttons being clicked
-    public void GetButtonInput()
-    {
-        if (theButton.CompareTag("Start"))
-        {
-            CurrentCase = "Start";
-            DoButtonAction();
-        }
-        if (theButton.CompareTag("Credits"))
-        {
-            CurrentCase = "Credits";
-            DoButtonAction();
-        }
-        if (theButton.CompareTag("Quit"))
-        {
-            CurrentCase = "Quit";
-            DoButtonAction();
-        }
-    }
+    public Canvas ButtonCanvas;
+    public GameObject ButtonObject;
+    public Text ButtonText;
 
     //The actions of the buttons
-    public string DoButtonAction()
+    public void DoButtonAction()
     {
-        switch (CurrentCase)
-        {
-            case "Start":
-                {
-                    Debug.Log("Start Button was Pressed");
-                    testText.text = "Start Button was Pressed";
-                    return CurrentCase;
-                }
-            case "Credits":
-                {
-                    Debug.Log("Credits Button was Pressed");
-                    testText.text = "Credits Button was Pressed";
-                    return CurrentCase;
-                }
-            case "Quit":
-                {
-                    Debug.Log("Quit Button was Pressed");
-                    testText.text = "Quit Button was Pressed";
-                    return CurrentCase;
-                }
-            default:
-                return null;
-        }
+        
 
     }
 
     //Create buttons for scene
     public void CreateButtons()
     {
-        GameObject startButton;
-        startButton = Instantiate(buttonObject);
+        GameObject startButton = Instantiate(ButtonObject);
         startButton.tag = "Start";
-        startButton.transform.SetParent(buttonCanvas.transform);
+        startButton.transform.SetParent(ButtonCanvas.transform);
         startButton.transform.position = new Vector3(300, 300, 0);
         startButton.GetComponentInChildren<Text>().text = "Start";
         startButton.name = "Start";
 
-        GameObject creditsButton;
-        creditsButton = Instantiate(buttonObject);
+        GameObject creditsButton = Instantiate(ButtonObject);
         creditsButton.tag = "Credits";
-        creditsButton.transform.SetParent(buttonCanvas.transform);
+        creditsButton.transform.SetParent(ButtonCanvas.transform);
         creditsButton.transform.position = new Vector3(500, 300, 0);
         creditsButton.GetComponentInChildren<Text>().text = "Credits";
         creditsButton.name = "Credits";
 
-        GameObject quitButton;
-        quitButton = Instantiate(buttonObject);
+        GameObject quitButton = Instantiate(ButtonObject);
         quitButton.tag = "Quit";
-        quitButton.transform.SetParent(buttonCanvas.transform);
+        quitButton.transform.SetParent(ButtonCanvas.transform);
         quitButton.transform.position = new Vector3(700, 300, 0);
         quitButton.GetComponentInChildren<Text>().text = "Quit";
         quitButton.name = "Quit";
-
     }
 
-    void Start()
+
+
+    public void Start()
     {
         CreateButtons();
     }
 
-    void Update()
+    public void Update()
     {
-        GetButtonInput();
+        
     }
-
-
 
 }
