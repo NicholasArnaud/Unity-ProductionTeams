932f1ad93cf7248b55b6e4e34a9b597898b90685
Nicholas Arnaud <cobrahead09@gmail.com>
2017-06-02 17:07:53 -0500

    work on rotation with player

--- Assets/Scripts/PlayerCameraBehaviour.cs
+++ Assets/Scripts/PlayerCameraBehaviour.cs
@@ -15,27 +15,34 @@ public class PlayerCameraBehaviour : MonoBehaviour
     private Vector3 offset;
     private float nearClip;
     private RaycastHit hit;
+    private float distanceBetween;
+
     // Use this for initialization
     void Start()
     {
         transform.rotation = Quaternion.Euler(CameraXRotation, CameraYRotation, 0);
         nearClip = GetComponent<Camera>().nearClipPlane;
         offset = transform.position - PlayerToFollow.transform.position;
+        distanceBetween = Vector3.Distance(PlayerToFollow.transform.position, transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = PlayerToFollow.transform.position + offset;
-        if (!Physics.Raycast(transform.position, Vector3.forward, out hit, 10))
+        transform.rotation = PlayerToFollow.transform.rotation;
+
+        if (!Physics.Raycast(transform.position, Vector3.forward, out hit, PlayerToFollow.transform.position.z - transform.position.z))
         {
-            nearClip = 0.3f;
+            if(nearClip != 0.3f)
+                GetComponent<Camera>().nearClipPlane = 0.3f;
         }
         else
         {
             var distance = hit.distance;
             var distanceToCenter = hit.collider.bounds.extents.z;
             nearClip = .3f + (distance + distanceToCenter * 2f);
+            GetComponent<Camera>().nearClipPlane = nearClip;
         }
     }
 
