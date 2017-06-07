c0bf1b29e61e202f4cbb82e63e177b3d6b99e745
BlaneTorregrossa <blanet97@cox.net>
2017-06-07 16:36:44 -0500

    .

---
+++ Assets/ButtonAnimate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnimate : MonoBehaviour
+{
+
+    private Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void MouseOver()
+    {
+        animator.SetTrigger("Hilighted");
+    }
+
+    public void Selected()
+    {
+        animator.SetTrigger("Pressed");
+    }
+}
--- Assets/Scripts/DisplayCreditsOnClick.cs
+++ Assets/Scripts/DisplayCreditsOnClick.cs
@@ -24,4 +24,6 @@ public class DisplayCreditsOnClick : MonoBehaviour
     {
         CreditsCamera.SetActive(false);
     }
+
+    
 }
