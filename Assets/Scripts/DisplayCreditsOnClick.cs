60b913df48c139aa902d39b13f8b911aec3f08fe
BlaneTorregrossa <blanet97@cox.net>
2017-06-07 12:58:51 -0500

    Made Buttons Work

---
+++ Assets/Scripts/DisplayCreditsOnClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DisplayCreditsOnClick : MonoBehaviour
+{
+    public GameObject MenuCamera;
+    public GameObject CreditsCamera;
+
+    public void SwitchToCredits()
+    {
+        MenuCamera.SetActive(false);
+        CreditsCamera.SetActive(true);
+    }
+
+    public void SwitchToMainMenu()
+    {
+        MenuCamera.SetActive(true);
+        CreditsCamera.SetActive(false);
+    }
+
+    void Start()
+    {
+        CreditsCamera.SetActive(false);
+    }
+}
--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,61 +1,72 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.Events;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using UnityEngine.Events;
 
-//For Buttons on Menu
-public class MenuController : MonoBehaviour
-{
-    [HideInInspector]
-    public Button TheButton;
+////For Buttons on Menu
+//public class MenuController : MonoBehaviour
+//{
+//    public Button TheButton;
+//    public Canvas ButtonCanvas;
+//    public GameObject ButtonObject;
+//    public Text ButtonText;
 
-    public Canvas ButtonCanvas;
-    public GameObject ButtonObject;
-    public Text ButtonText;
-
-    //The actions of the buttons
-    public void DoButtonAction()
-    {
-        
-
-    }
-
-    //Create buttons for scene
-    public void CreateButtons()
-    {
-        GameObject startButton = Instantiate(ButtonObject);
-        startButton.tag = "Start";
-        startButton.transform.SetParent(ButtonCanvas.transform);
-        startButton.transform.position = new Vector3(300, 300, 0);
-        startButton.GetComponentInChildren<Text>().text = "Start";
-        startButton.name = "Start";
-
-        GameObject creditsButton = Instantiate(ButtonObject);
-        creditsButton.tag = "Credits";
-        creditsButton.transform.SetParent(ButtonCanvas.transform);
-        creditsButton.transform.position = new Vector3(500, 300, 0);
-        creditsButton.GetComponentInChildren<Text>().text = "Credits";
-        creditsButton.name = "Credits";
-
-        GameObject quitButton = Instantiate(ButtonObject);
-        quitButton.tag = "Quit";
-        quitButton.transform.SetParent(ButtonCanvas.transform);
-        quitButton.transform.position = new Vector3(700, 300, 0);
-        quitButton.GetComponentInChildren<Text>().text = "Quit";
-        quitButton.name = "Quit";
-    }
+//    public GameObject startButton;
+//    public GameObject creditsButton;
+//    public GameObject quitButton;
+//    public Button startBtn;
+//    public Button creditsBtn;
+//    public Button quitBtn;
 
 
+//    //Create buttons for scene
+//    public void CreateButtons()
+//    {
 
-    public void Start()
-    {
-        CreateButtons();
-    }
+//        //startButton = Instantiate(ButtonObject);
+//        //startButton.tag = "Start";
+//        //startButton.transform.SetParent(ButtonCanvas.transform);
+//        //startButton.transform.position = new Vector3(300, 300, 0);
+//        //startButton.GetComponentInChildren<Text>().text = "Start";
+//        //startButton.name = "Start Button";
+//        //startBtn = TheButton.GetComponent<Button>();
+//        //startBtn.tag = "Start";
+//        //startBtn.onClick.AddListener(SwitchGameScene);
 
-    public void Update()
-    {
-        
-    }
 
-}
+//        //creditsButton = Instantiate(ButtonObject);
+//        //creditsButton.tag = "Credits";
+//        //creditsButton.transform.SetParent(ButtonCanvas.transform);
+//        //creditsButton.transform.position = new Vector3(500, 300, 0);
+//        //creditsButton.GetComponentInChildren<Text>().text = "Credits";
+//        //creditsButton.name = "Credits Button";
+//        //creditsBtn = TheButton.GetComponent<Button>();
+//        //creditsBtn.tag = "Credits";
+//        //creditsBtn.onClick.AddListener(DisplayCredits);
+
+
+//        //quitButton = Instantiate(ButtonObject);
+//        //quitButton.tag = "Quit";
+//        //quitButton.transform.SetParent(ButtonCanvas.transform);
+//        //quitButton.transform.position = new Vector3(700, 300, 0);
+//        //quitButton.GetComponentInChildren<Text>().text = "Quit";
+//        //quitButton.name = "Quit Button";
+//        //quitBtn = TheButton.GetComponent<Button>();
+//        //quitBtn.tag = "Quit";
+//        //quitBtn.onClick.AddListener(CloseApplication);
+//    }
+
+
+
+//    public void Start()
+//    {
+//        CreateButtons();
+//    }
+
+//    public void Update()
+//    {
+
+//    }
+
+//}
