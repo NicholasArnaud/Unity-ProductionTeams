5963995bbff55d8f1c21a31042916b76e5838c28
Nicholas Arnaud <cobrahead09@gmail.com>
2017-06-09 11:17:26 -0500

    edit movement scripts

--- Assets/Scripts/PlayerMovementBehaviour.cs
+++ Assets/Scripts/PlayerMovementBehaviour.cs
@@ -1,27 +1,71 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovementBehaviour : MonoBehaviour
 {
-    private Rigidbody rb;
-    [Range(0f, 100f)]public float speed;
+    //private Rigidbody rb;
+   // [Range(0f, 100f)]public float speed;
+
+    public float maxAngle = 30f;
+    public float maxTorque = 300f;
+    public float brakeTorque = 30000f;
+
+    public float criticalSpeed = 5f;
+    public int stepsBelow = 5;
+    public int stepsAbove = 1;
+
+
+    private WheelCollider[] m_Wheels;
 
     // Use this for initialization
     void Start ()
 	{
-	    rb = GetComponent<Rigidbody>();
+	    //rb = GetComponent<Rigidbody>();
+
+
+
+	    m_Wheels = GetComponentsInChildren<WheelCollider>();
+
+	    for (int i = 0; i < m_Wheels.Length; i++)
+	    {
+	        var wheel = m_Wheels[i];
+	    }
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	    float moveHorizontal = Input.GetAxis("Horizontal");
-	    float moveVertical = Input.GetAxis("Vertical");
-        rb.AddForce(new Vector3(transform.forward.x, 0, transform.forward.z) * (moveVertical * speed));
+	    m_Wheels[0].ConfigureVehicleSubsteps(criticalSpeed, stepsBelow, stepsAbove);
+
+        float angle = maxAngle * Input.GetAxis("Horizontal");
+	    float torque = maxTorque * Input.GetAxis("Vertical");
+
+	    float handBrake = Input.GetKey(KeyCode.X) ? brakeTorque : 0;
+
+	    foreach (WheelCollider wheel in m_Wheels)
+	    {
+	        if (wheel.transform.localPosition.z > 0)
+	            wheel.steerAngle = angle;
+
+	        if (wheel.transform.localPosition.z < 0)
+	            wheel.brakeTorque = handBrake;
+
+	        if (wheel.transform.localPosition.z < 0)
+	        {
+	            wheel.motorTorque = torque;
+	        }
+
+	        if (wheel.transform.localPosition.z >= 0)
+	        {
+	            wheel.motorTorque = torque;
+	        }
+
+        }
+
+        //rb.AddForce(new Vector3(transform.forward.x, 0, transform.forward.z) * (moveVertical * speed));
         //rb.velocity = new Vector3(transform.forward.x, 0, transform.forward.z) * (moveVertical * speed);
         //rb.velocity = transform.forward * (moveVertical * speed);
-	    transform.Rotate(0, moveHorizontal, 0);
+	    //transform.Rotate(0, moveHorizontal, 0);
 	}
 }
